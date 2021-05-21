46c20ff733c3e26e9ae8c889caa6f568340ad6fb
Eric Riley <eriley92@gmail.com>
2021-05-20 21:33:42 -0500

    basic setup including logging and error handling

---
+++ src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/whats-for-dinner',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/whats-for-dinner-test'
+
+  };
